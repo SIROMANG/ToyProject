c4010288f4aeb79b7193aa28dfbf52bffdbee96e
yuseungchul <yoosc14@gmail.com>
2022-10-28 14:13:38 +0900

    가입,로그인,로그아웃 완료 테스트필요

--- app/src/main/java/com/greedy/toyproject/MainActivity.kt
+++ app/src/main/java/com/greedy/toyproject/MainActivity.kt
@@ -1,11 +1,76 @@
 package com.greedy.toyproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.greedy.toyproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+   private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        binding.join.setOnClickListener {
+            startActivity(Intent(this, JoinActivity::class.java))
+        }
+
+        binding.btnLogin.setOnClickListener{
+            val email = binding.emeil.text.toString()//이메일버튼으로 접근
+            val password = binding.password.text.toString()
+
+            /* 여러 유효성 검사 추가할 수 있음 */
+            if (email.isNotEmpty() && password.isNotEmpty()) { //ㅣ지금은 비어있는지만 확인
+                signIn(email, password)
+            } else {
+                Toast.makeText(this, "email과 password를 입력해주세요", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
+    }
+
+    private fun signIn(email: String, password: String) {
+
+        auth.signInWithEmailAndPassword(email, password) //회원가입 요청 (필요한 요소넘기기)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    Toast.makeText(this, "로그인 되었습니다.", Toast.LENGTH_SHORT).show()
+                    moveResultPage()
+                } else {
+
+                    Toast.makeText(this, "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show()
+
+                }
+
     }
 }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser //currentUser 유거가 있다면 리졸트창으로 이동 리졸트에서 직접로그인을 요청
+        if(currentUser != null){
+            moveResultPage() //두번 사용하기 떄문에 별도로 선언함
+        }
+    }
+
+    /*로그인 이미 되어 있는 상태 또는 로그인에 성공하는 상태에 호출할 메소드 */
+    fun moveResultPage() { //로그인 성공 or 로그인 된상태일 때
+        startActivity(Intent(this, MypageActivity::class.java))
+        finish()
+    }
+
+
+    }
---
+++ app/src/main/java/com/greedy/toyproject/MypageActivity.kt
@@ -0,0 +1,32 @@
+package com.greedy.toyproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.greedy.toyproject.databinding.ActivityMypageBinding
+
+class MypageActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val binding by lazy { ActivityMypageBinding.inflate(layoutInflater) }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.currentUser.text = "${auth.currentUser?.email}"
+        binding.btnLogout.setOnClickListener{
+            val intent = Intent (this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+
+            auth.signOut()
+        }
+    }
+}
